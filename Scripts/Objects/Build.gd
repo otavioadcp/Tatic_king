8472fdc1e9a062e34cac0bc5b8e3f51268cc3b04
Otavio Pimentel <otavioadcp@gmail.com>
2026-01-16 10:42:22 -0400

    Update: add 'unit' and 'build' classes

---
+++ Scripts/Objects/Build.gd
@@ -0,0 +1,17 @@
+class_name Build extends Node
+
+const value: int = 0
+const hp: int = 0
+const has_production: bool = false
+const max_level: int = 1
+const shield: float = 0
+const hex_limitations: Array[String] = [] #quais hex não pode ser construído
+
+func build():
+	return "not implemented"
+	
+func demolish():
+	return "not implemented"
+	
+func upgrade():
+	return "not implemented"
---
+++ Scripts/Objects/Unit.gd
@@ -0,0 +1,17 @@
+class_name Unit extends Node
+
+const MOVEMENT: int = 0
+const VALUE: float = 0.0
+const DAMAGE: float = 0.0
+const RANGE: float = 1
+const SHIELD: float = 0.0
+const EXPERIENCE: float = 0
+
+func move(): 
+	return "not implemented"
+	
+func attack():
+	return "not implemented"
+
+func take_damage():
+	return "not implemented"
